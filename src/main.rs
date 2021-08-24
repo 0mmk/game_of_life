ad770acaf9bdeefb294d7e061c1c58d5b30e1dca
Mehmet Melih Karakaya <karakaya.mehmet.melih@gmail.com>
2021-08-24 05:10:11 +0300

    implemented cmd args and error reporting

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,30 @@ mod game_conf;
 use game::Game;
 use game_conf::GameConf;
 
-use std::fs;
+use std::{env, fs, process};
 
 fn main() {
-    let filename = "game.txt";
-    let contents =
-        fs::read_to_string(filename).expect(format!("file `{}` not found", filename).as_str());
+    let contents = match read_gamefile() {
+        Ok(val) => val,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1)
+        }
+    };
     let conf = GameConf::new(&contents);
     let mut game = Game::new(conf);
     game.start();
+}
+
+fn read_gamefile() -> Result<String, String> {
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        Err(format!("Usage: {} <filename>", args[0]))
+    } else {
+        let filename = &args[1];
+        match fs::read_to_string(filename) {
+            Ok(contents) => Ok(contents),
+            Err(err) => Err(err.to_string()),
+        }
+    }
 }
